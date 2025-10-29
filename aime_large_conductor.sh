7ab56d91fb3fa4486009067757b8adc6eea9c06d
bxing <bxing@groq.com>
2025-10-29 19:42:38 -0400

    simple eval stuff

--- aime_large_conductor.sh
+++ aime_large_conductor.sh
@@ -3,5 +3,5 @@
 for i in {1..10}
 do
         echo "run $i"
-        CONDUCTOR_IP="$1" /opt/homebrew/bin/python3.13 -m simple-evals.simple_evals --model emberglow-large_low --eval aime --n-repeats 8 --n-threads 8
+        CONDUCTOR_IP="$1" python3.14 -m simple-evals.simple_evals --model emberglow-large_low --eval aime2025 --n-repeats 8 --n-threads 8
 done
