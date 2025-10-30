9807bba7ecb9121c988976fea690b8a59953996a
bxing <bxing@groq.com>
2025-10-30 10:50:38 -0400

    30

--- gpqa_large_conductor.sh
+++ gpqa_large_conductor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {1..10}
+for i in {1..30}
 do
         echo "run $i"
         CONDUCTOR_IP="$1" python3.14 -m simple-evals.simple_evals --model emberglow-large_low --eval gpqa --n-repeats 8 --n-threads 16
